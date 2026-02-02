6e8f40861922a5d4f0d2d1921a18688bacb5b2b4
Ayodélé BANSOU <ayodelebansou@MacBook-Air-de-Ayodele.local>
2026-02-02 16:58:34 +0100

    [ADD] iris.csv new version

--- clean_data.py
+++ clean_data.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 
 # 1. Lecture du fichier CSV original
@@ -8,11 +7,12 @@ df = pd.read_csv('iris.csv')
 df_clean = df.copy()
 
 # 3. Suppression des colonnes inutiles pour la consigne
-columns_to_delete = ['petal_length', 'petal_width', 'species']  
+columns_to_delete = ['petal_length', 'petal_width']  
 df_clean.drop(columns=columns_to_delete, inplace=True)
 
 # 4. Sauvegarde de la version nettoyée dans un nouveau fichier
 df_clean.to_csv('iris_cleaned.csv', index=False)
 
-print("Le fichier dupliqué 'iris_cleaned.csv' a été créé !")
+print("Le fichier dupliqué a été créé !")
 print("Colonnes restantes :", df_clean.columns.tolist())
+
